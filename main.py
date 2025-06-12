af999756020c264e697ce6de31d4ccfa01ac4d55
Nora_Meadows1121 <foxwolf445@gmail.com>
2025-06-12 13:53:50 -0600

    Added function to count words | Fixed an issue with frankenstein being printed twice

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
-    return file_contents
+    words = file_contents.split()
+    return len(words)
+
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    print(f"{get_book_text("books/frankenstein.txt")} words found in the document")
 
 main()
+
