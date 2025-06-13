9bae36c3c456a363a754ff4683feb10bc806066e
Nora_Meadows1121 <foxwolf445@gmail.com>
2025-06-13 02:35:32 -0600

    cleaned up code by removing redundant variables | fixed typo in instructions

--- main.py
+++ main.py
@@ -6,26 +6,18 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents
 
-def mai():
-    print(sys.argv)
-
 def main():
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     else:
-        text = get_book_text(sys.argv[1])
-        num = get_num_words(text)
-        letters = get_letters(text)
-        sorted_letters = sort(letters)
         print("============ BOOKBOT ============")
         print(f"Analyzing book found at {sys.argv[1]}")
         print("----------- Word Count ----------")
-        print(f"Found {num} total words")
+        print(f"Found {get_num_words(get_book_text(sys.argv[1]))} total words")
         print("--------- Character Count -------")
-        for item in sorted_letters:
+        for item in sort(get_letters(get_book_text(sys.argv[1]))):
             print(f"{item['char']}: {item['num']}")
         print("============= END ===============")
 
 main()
-
